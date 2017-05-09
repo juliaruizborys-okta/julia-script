8c388ad27f2487997b62394f84a13f6549d793c7
Camden Bickel <cam@bickel.io>
2017-05-08 22:18:48 -0700

    fixed title and options

--- src/MainEditor.js
+++ src/MainEditor.js
@@ -4,17 +4,34 @@ import { EditorState } from 'draft-js';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './main.css';
 
+//TODO refactor
+const toolbar = {
+    options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link', 'embedded', 'emoji', 'image', 'remove', 'history'],
+    fontFamily: {
+        options: ['Arial', 'Courier']
+    },
+};
+
 export default class MainEditor extends React.Component {
     constructor(props) {
-            super(props);
-            this.state = {editorState: EditorState.createEmpty()};
-        this.onChange = (editorState) => this.setState({editorState});
-          
+        super(props);
+        this.state = {editorState: EditorState.createEmpty()};
+        this.onEditorStateChange = this.onEditorStateChange.bind(this);
     }
+
+    onEditorStateChange(editorState) {
+        this.setState({editorState});
+    }
+
     render() {
+        const editorState = this.state.editorState;
         return (
             <div className="editor-container">
-                <Editor editorState={this.state.editorState} onEditorStateChange={this.onChange} />
+                <Editor 
+                    toolbar={toolbar}
+                    editorState={editorState} 
+                    onEditorStateChange={this.onEditorStateChange} 
+                />
             </div>
         );
     }
